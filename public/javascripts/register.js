74084541f7deb2d16a6e207ac2ec218cd425ece3
Charles <charlesdawes07@gmail.com>
2018-03-20 13:54:54 -0400

    registration

--- public/javascripts/register.js
+++ public/javascripts/register.js
@@ -15,6 +15,46 @@ $("#btnSubmit").on('click', function(e){
       toastr.error("Your passwords do not match", "Password Error", {timeOut: 5000, positionClass: 'toast-bottom-center'});
     } else {
       //submit!
+      if (password.length < 6 || !hasUpperCase(password)){
+        toastr.error("Your password does not match the password requirements.  Please try again.", "Password Error", { timeOut: 5000, positionClass: 'toast-bottom-center'});
+      }  else {
+        var data = {
+          firstName: firstName,
+          lastName: lastName,
+          password: confPassword,
+          phoneNumber: phoneNumber
+        }
+        $.post('http://localhost:3000/api/post/create/user', data, function(result){
+          console.log(result.created);
+          if (result.created === true){
+            toastr.options.onHidden = function(){window.location.href='http://localhost:3000';}
+            toastr.success("Your account has been created. Redirecting to Home page.", "Account Created!",{ timeOut: 3000, positionClass: 'toast-bottom-center'});
+          } else {
+            toastr.error("An error has occurred, please try again!", "Error", { timeOut: 3000, positionClass: 'toast-bottom-center'})
+          }
+        });
+      }
     }
   }
 })
+
+$("#txtPhoneNumber").on('blur', function(){
+  $("#btnSubmit").attr("disabled","disabled");
+  var string = $("#txtPhoneNumber").val();
+  string = string.replace(/-/g,'').replace('(','').replace(')','');
+  $("#txtPhoneNumber").val(string);
+  if (!$.isNumeric(string)){
+    $("#txtPhoneNumber").parent().parent().addClass("has-danger");
+    toastr.error("Please enter a valid phone number.","",{ timeOut: 3000, positionClass: 'toast-bottom-center'});
+  } else {
+    $("#btnSubmit").removeAttr("disabled");
+    $("#txtPhoneNumber").parent().parent().removeClass("has-danger");
+  }
+})
+
+function hasUpperCase(str){
+  if (str.toLowerCase() != str){
+    return true;
+  }
+  return false;
+}
--- routes/api/v1.js
+++ routes/api/v1.js
@@ -1,6 +1,21 @@
 var express = require('express');
 var router = express.Router();
 var pgp = require('pg-promise')();
+var jwt = require('jsonwebtoken');
+var jsSHA = require("jssha");
+
+//JWT Config
+jwt.verifyToken = function(reqToken){
+  return verifyToken(reqToken);
+};
+var verifyToken = function(reqToken){
+  if (token === reqToken){
+    return true;
+  } else {
+    return false;
+  }
+}
+
 
 //SETUP DB CONNECTION
 const con = {
@@ -26,22 +41,75 @@ router.get('/get/user/:id', function(req, res, next) {
 
 
 //POST REQUESTS SECTION
+router.post('/post/create/user', function(req, res, next) {
+  db.oneOrNone("Select * from public.get_user_by_phone_number($1::text)", [req.body.phoneNumber]).then((data) => {
+    if (data == null){
+      createUser(req, res, next);
+    } else {
+      res.json({created: 'user already exists'});
+    }
+  })
+});
 
 
-
-
+//API FUNCTIONS
+  //GET
 function getUserById(req, res, next){
   if (!isNaN(req.params.id)){
-    db.one('SELECT * from public.get_user($1)', [req.params.id])
+    db.oneOrNone('SELECT * from public.get_user($1)', [req.params.id])
       .then(function(data){
+        if (data === null){
+          res.json({message:"user not found"})
+        }
         res.send(data);
+
     });
   } else {
-    db.one('SELECT * from public.get_user_by_token($1)', [req.params.id])
+    db.oneOrNone('SELECT * from public.get_user_by_token($1)', [req.params.id])
       .then(function(data){
         res.send(data);
     });
   }
+
+}
+
+
+  //POST
+function createUser(req, res, next){
+  //console.log(req.body.firstName);
+  let firstName = req.body.firstName;
+  let lastName = req.body.lastName;
+  let hashedPassword = hashString(req.body.password);
+  let token = genToken(firstName + lastName);
+  let phoneNumber = req.body.phoneNumber;
+  const createUser = {
+    name: 'get-user',
+    text: 'SELECT * from public.put_user($1::text, $2::text, $3::text, $4::text, $5::text)',
+    values: [firstName, lastName, phoneNumber, token, hashedPassword],
+    rowMode: 'array'
+  };
+  db.any(createUser).then((data) => {
+    res.json({created: data[0][0]});
+  })
+  //console.log(firstName, lastName, hashedPassword, token, phoneNumber);
+
+}
+
+
+
+//HELPER FUNCTIONS
+
+function hashString(value){
+  var shaObj = new jsSHA("SHA-512", "TEXT");
+  shaObj.update(value);
+  return shaObj.getHash("HEX");
+}
+
+function genToken(value){
+  return jwt.sign(value,  Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15))
+}
+
+function doesUserExist(number){
 
 }
 
